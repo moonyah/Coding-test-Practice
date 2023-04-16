caebefc8717a3789a27ad02f95e687f776545f86
MunYong <51106050+moonyah@users.noreply.github.com>
2023-04-17 00:39:30 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,21 +1,11 @@
 function solution(s) {
     var answer = '';
-    answer = s.toUpperCase().split(" ");
-    var arr = [];
-    for(let i = 0; i < answer.length; i++){
-        if(i >= 0){
-            arr.push(answer[i][0]);
-        }
-         arr.push(answer[i].toLowerCase().slice(1)); 
+    for (let i = 0; i < s.length; i++) {
+      if (i === 0 || s[i-1] === " ") {
+        answer += s[i].toUpperCase();
+      } else {
+        answer += s[i].toLowerCase();
+      }
     }
-    
-    var arr2 = [];
-    for(let i = 0; i < arr.length; i++){
-        if(i%2 !== 0 && i !== arr.length-1){
-            arr2.push(arr[i]+' ');
-        }else{
-            arr2.push(arr[i]);
-        }
-    }
-    return arr2.join("");
+    return answer;
 }
