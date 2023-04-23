9130bb0f8a5ae10dcb3b21f35f50913786ee637e
MunYong <51106050+moonyah@users.noreply.github.com>
2023-04-23 17:22:06 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,11 @@
+function solution(str_list, ex) {
+    var answer = [];
+    for(let i = 0; i < str_list.length; i++){
+        if(str_list[i].includes(ex)){
+             answer.push("");
+        }else{
+            answer.push(str_list[i]);
+        }
+    }
+    return answer.join('');
+}
