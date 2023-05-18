a1344aea2e822be042a463c5e6de1a6ad46d4689
MunYong <51106050+moonyah@users.noreply.github.com>
2023-05-18 20:11:23 +0900

    [D3] Title: 24시간, Time: 205 ms, Memory: 59,008 KB -BaekjoonHub

---
+++ SWEA/D3/12368. 24시간/24시간.py
@@ -0,0 +1,6 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    li = list(map(int, input().split(' ')))
+    answer = (li[0]+li[1])%24
+    print('#%d %d'%(test_case, answer))
