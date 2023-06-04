495d4e14ec8ba57d566ff3ae4c4f9540e235b3cd
MunYong <51106050+moonyah@users.noreply.github.com>
2023-06-04 16:20:58 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 2.07 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    var answer = [];
+    for(let i = 0; i < queries.length;i++){
+        let s = queries[i][0];
+        let e = queries[i][1];
+        for(let j = s; j <= e; j++){
+            arr[j] += 1;
+        }
+    }
+    return arr;
+}
