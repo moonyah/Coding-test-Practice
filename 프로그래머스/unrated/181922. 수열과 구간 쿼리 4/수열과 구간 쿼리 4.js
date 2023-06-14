c571d662ed8a4ed82d98df2d95ca592106e53cae
MunYong <51106050+moonyah@users.noreply.github.com>
2023-06-14 22:14:36 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 8.19 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    var answer = [];
+    for(let i = 0; i < queries.length;i++){
+        for(let j = queries[i][0]; j <= queries[i][1]; j++){
+            if(j%queries[i][2]==0){
+                arr[j] += 1;
+            }
+        }
+    }    
+    return arr;
+}
