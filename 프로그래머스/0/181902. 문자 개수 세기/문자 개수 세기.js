b83471f545d1e07c3963b6cc43a607876751b4d8
MunYong <51106050+moonyah@users.noreply.github.com>
2024-02-28 16:20:43 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    var answer = [];
+    let arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z']
+    for(let i in arr){
+        answer.push(my_string.split('').filter( el => arr[i] === el).length)
+    }
+    return answer;
+}
