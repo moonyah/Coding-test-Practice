0b21684fdc3e3931578c949aad336bfbb99c397d
MunYong <51106050+moonyah@users.noreply.github.com>
2024-02-29 17:36:50 +0900

    [level 0] Title: 배열 만들기 6, Time: 6.49 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    var answer = [];
+    let i = 0;
+    let stk = []
+    while(i < arr.length){
+        if(stk.length == 0 || (stk.length !== 0 && stk[stk.length-1] !== arr[i])){
+            stk.push(arr[i]);
+            i++;
+        }
+        if(stk.length !== 0 && stk[stk.length-1] == arr[i]){
+            stk.pop();
+            i++;
+        }
+    }
+    return stk.length == 0 ? [-1] : stk;
+}
