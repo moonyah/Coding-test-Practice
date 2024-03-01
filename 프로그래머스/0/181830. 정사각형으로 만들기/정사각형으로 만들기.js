7bcdf7ee202557b5bbc18720c892028a00552363
MunYong <51106050+moonyah@users.noreply.github.com>
2024-03-01 21:39:50 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.06 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,26 +1,20 @@
-// function solution(arr) {
-//     var answer = [];
-//     let 행 = arr.length;
-//     let 열 = arr[0].length;
-//     if(행 > 열){ // 끝에 0을 추가해야 하는 경우
-//         for(let i in arr){
-//             let zeroArr = new Array(행).fill(0);
-//             answer.push(zeroArr.map((el, index) => index < 열 ? el = arr[i][index] : el = 0))
-//         }
-//     }else if(행 < 열){ // 0인 열 추가
-//         let zeroArr = new Array(열).fill(0);
-//         answer = [...arr];
-//         for(let i = 0; i < (열-arr.length); i++){
-//             answer.push(zeroArr)
-//         }
-//     }else{ // 행 수 = 열 수
-//         answer = arr;
-//     }
-//     return answer;
-// }
 function solution(arr) {
-    let n = Math.max(arr.length, arr[0].length);
-    for (let a of arr) while (a.length<n) a.push(0);
-    while (arr.length < n) arr.push(Array(n).fill(0));
-    return arr;
+    var result = [];
+    let 행 = arr.length;
+    let 열 = arr[0].length;
+    if(행 > 열){ // 끝에 0을 추가해야 하는 경우
+        for(let i in arr){
+            let zeroArr = new Array(행).fill(0);
+            result.push(zeroArr.map((el, index) => index < 열 ? el = arr[i][index] : el = 0))
+        }
+    }else if(행 < 열){ // 0인 열 추가
+        let zeroArr = new Array(열).fill(0);
+        result = [...arr];
+        for(let i = 0; i < (열-arr.length); i++){
+            result.push(zeroArr)
+        }
+    }else{ // 행 수 = 열 수
+        result = arr;
+    }
+    return result;
 }
