fda412dbb7f7dad42806e267e37adf81c76f696f
MunYong <51106050+moonyah@users.noreply.github.com>
2024-03-16 03:21:04 +0900

    [level 0] Title: 등수 매기기, Time: 0.09 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,14 +1,9 @@
 function solution(score) {
     var answer = [];
-    
     let meanArr = [];
     for(let i of score){
-        meanArr.push((i[0]+i[1])/2); // [75,75,40,95,95,100,20]
+        meanArr.push((i[0]+i[1])/2);
     }
-    
     answer = meanArr.map(a => a = [...meanArr].sort((a, b)=>(b-a)).indexOf(a)+1)
-    
-    
-
     return answer;
 }
