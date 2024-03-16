410b995b3372d4685f1bcbb5418e26d5f116328b
MunYong <51106050+moonyah@users.noreply.github.com>
2024-03-17 03:16:32 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,9 @@
+function solution(chicken) {
+    var answer = 0;
+    let coupon = chicken;   
+    while(coupon >= 10){
+        answer += Math.floor(coupon/10);
+        coupon = Math.floor(coupon/10) + coupon%10;
+    }
+    return answer;
+}
