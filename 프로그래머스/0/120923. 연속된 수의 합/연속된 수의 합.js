3e863b8acbafa2733578cad44bbae9ebeb4a8b9e
MunYong <51106050+moonyah@users.noreply.github.com>
2024-03-22 15:34:11 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,9 +1,4 @@
 function solution(num, total) {
-    var answer = [];
-    let 처음수 = Math.ceil(total/num - num/2);
-    for(let i = 0; i < num; i++){
-        answer.push(처음수);
-        처음수 = 처음수 + 1;
-    }
-    return answer;
+    var min = Math.ceil(total/num - num/2);
+    return new Array(num).fill(0).map((el,i)=>{return i+min;});
 }
