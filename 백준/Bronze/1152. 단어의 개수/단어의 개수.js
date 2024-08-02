bb1be81551b2266fe9229e9543bbc96bf97cc876
MunYong <51106050+moonyah@users.noreply.github.com>
2024-08-02 15:37:15 +0900

    [Bronze II] Title: 단어의 개수, Time: 200 ms, Memory: 23032 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const data = input[0].trim().split(" ");
+if (data == "") {
+  console.log(0);
+} else {
+  console.log(data.length);
+}
