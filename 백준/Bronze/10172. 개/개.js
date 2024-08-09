6cd5f1a9c341e1973e584291a0c7e8d34cb08472
MunYong <51106050+moonyah@users.noreply.github.com>
2024-08-09 18:20:28 +0900

    [Bronze V] Title: 개, Time: 108 ms, Memory: 9276 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.js
+++ 백준/Bronze/10172. 개/개.js
@@ -1,5 +1,5 @@
-console.log(`|\\_/|`);
-console.log(`|q p|   /}`);
-console.log(`( 0 )"""\\`);
-console.log(`|"^"\`    |`);
-console.log(`||_/=\\\\__|`);
+console.log(`|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"\`    |
+||_/=\\\\__|`);
