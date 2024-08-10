e3cbd34d15fe6d532e7e4d0f5ff581916735a781
MunYong <51106050+moonyah@users.noreply.github.com>
2024-08-10 19:07:29 +0900

    [Silver III] Title: 주유소, Time: 340 ms, Memory: 54468 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,40 +1,29 @@
-// n번째 도시까지 보다 싸면 n번의 거리 합만큼 기름 넣기
-// 현재 도시보다 싼 곳이 나오기 전까지 거리를 갈 수 있는 기름 넣기
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-let cityNum = Number(input[0]);
-let oil = input[1].split(" ").map(Number);
-let oilPrice = input[2].split(" ").map(Number);
-// console.log(cityNum, oil, oilPrice);
+let cityNum = BigInt(input[0]);
+let distanceArr = input[1].split(" ").map((x) => BigInt(x));
+let cityOilPriceArr = input[2].split(" ").map((x) => BigInt(x));
 
-// curr도시에서 다음 도시, 다음 도시로 가다가 더 가격이 싼 곳이 나올 때까지 간다.
-// 더 싼 곳이 나오면 도로 몇개를 갔는지 계산한다. curr도시는 업데이트 된다.
-// 총 km와 curr의 oilPrice를 곱해서 answer에 합한다.
-// 다음은 현재 있는 도시에서 시작한다.
+let currIndex = 0n;
+let price = 0n;
+let distances = 0n;
 
-let currCityIndex = 0;
-let roadStack = 0;
-let answer = 0;
-for (let i = 0; i < cityNum; i++) {
-  // console.log(oilPrice[currCityIndex], oilPrice[i]);
-  roadStack++;
-  // console.log(roadStack);
-  if (oilPrice[i] < oilPrice[currCityIndex] || i == cityNum - 1) {
-    // 더 싼 곳을 발견했을 때
-    // console.log(
-    //   "여기 oilPrice가 더 싸거나 마지막 도시라서 멈췄어요.",
-    //   oilPrice[i]
-    // );
-    if (currCityIndex == 0) roadStack--;
-    // console.log("길 이만큼 갔어요", roadStack);
+for (let i = 0n; i < cityNum - 1n; i++) {
+  distances += distanceArr[BigInt(i)]; // 누적 거리
 
-    let roadOil = oil
-      .splice(0, roadStack)
-      .reduce((a, b) => a + b * oilPrice[currCityIndex], 0);
-    // console.log("roadOil", roadOil);
-    answer += roadOil;
-    roadStack = 0;
-    currCityIndex = i;
+  // 현재 도시의 기름 가격보다 다음 도시의 기름 가격이 더 저렴하거나 마지막 도시일 경우
+  if (
+    cityOilPriceArr[BigInt(currIndex)] > cityOilPriceArr[BigInt(i) + 1n] ||
+    i === cityNum - 2n
+  ) {
+    price += cityOilPriceArr[BigInt(currIndex)] * distances;
+    distances = 0n; // 거리 초기화
+    currIndex = i + 1n;
   }
 }
-console.log(answer);
-// https://www.acmicpc.net/problem/13305
+
+// 마지막 구간 처리
+if (distances > 0n) {
+  price += cityOilPriceArr[BigInt(currIndex)] * distances;
+}
+
+console.log(price.toString());
