560dbad982ea1f3fa0fc6e483a055b80d7227ebb
MunYong <51106050+moonyah@users.noreply.github.com>
2024-08-12 18:25:57 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 9532 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,6 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let num = Number(input[0]);
+for (let i = 1; i <= num; i++) {
+  let [a, b] = input[i].split(" ").map(Number);
+  console.log(`Case #${i}: ${a} + ${b} = ${a + b}`);
+}
