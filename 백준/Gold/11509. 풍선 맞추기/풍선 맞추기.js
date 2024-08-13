54382f0ba64bf1b30430d6c4039e45a0938e56f5
MunYong <51106050+moonyah@users.noreply.github.com>
2024-08-13 17:09:32 +0900

    [Gold V] Title: 풍선 맞추기, Time: 556 ms, Memory: 103208 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.js
@@ -0,0 +1,17 @@
+// 각각의 높이에 대해서 화살의 개수를 count
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let data = input[1].split(" ").map(Number); // 모든 풍선의 위치 정보 입력받기
+let arrow = new Array(1000001).fill(0); // 각 높이에 화살이 몇 개 있는지
+let result = 0;
+for (let x of data) {
+  if (arrow[x] > 0) {
+    // 해당 높이에 화살이 있다면
+    arrow[x] -= 1;
+    arrow[x - 1] += 1;
+  } else {
+    // 해당 높이에 화살이 없다면
+    arrow[x - 1] += 1;
+    result += 1; // 화살 쏘기
+  }
+}
+console.log(result);
