dc3a49492194b98ae81fa910f91562801b9c5187
MunYong <51106050+moonyah@users.noreply.github.com>
2024-10-04 14:59:06 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.40 ms
+메모리: 33.5 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 04일 14:54:10
+2024년 10월 04일 14:59:04
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,11 +1,11 @@
 function solution(n) {
     var answer = 0;
-    
-    let dp =[1, 2];
-    // n번 째 칸에 도달하는 방법: (n-1)번째 칸에 도달하는 방법 + (n-2)번째 칸에 도달하는 방법
-    for(let i = 2; i < n; i++){
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    var dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (var i = 3; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;  // 전체 합에 대해 나머지 연산 적용
     }
-    answer = dp[n-1];
-    return answer;
+    answer = dp[n];
+    return answer % 1234567;  // 결과에 다시 한 번 나머지 연산
 }
