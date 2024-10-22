29f5154797eb84b2cf5cda3633568f964ad61a0a
MunYong <51106050+moonyah@users.noreply.github.com>
2024-10-23 02:26:08 +0900

    [Silver II] Title: N과 M (12), Time: 152 ms, Memory: 10456 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.js
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -1,25 +1,27 @@
 const input = require("fs").readFileSync("dev/stdin").toString().split("\n");
-const [N, M] = input[0].split(" ").map((a) => Number(a));
+const [N, M] = input[0].split(" ").map(Number);
 const arr = input[1]
   .split(" ")
-  .map((a) => Number(a))
+  .map(Number)
   .sort((a, b) => a - b);
 
-let tempArr = [];
-function recursion(depth, line, start) {
+let result = [];
+let sequence = [];
+
+function recursion(depth, start) {
   if (depth === M) {
-    tempArr.push(line.trim());
+    result.push(sequence.join(" "));
     return;
   }
+  let prev = -1; // 중복을 방지하기 위한 변수
   for (let i = start; i < N; i++) {
-    let lineBuf = line;
-    line += arr[i] + " ";
-    recursion(depth + 1, line, i);
-    line = lineBuf;
+    if (arr[i] === prev) continue; // 이전 값과 동일하면 건너뜀
+    sequence.push(arr[i]);
+    recursion(depth + 1, i);
+    sequence.pop();
+    prev = arr[i]; // 이전 값을 현재 값으로 업데이트
   }
 }
-recursion(0, "", 0);
-let set = new Set(tempArr);
-for (let i of set) {
-  console.log(i);
-}
+
+recursion(0, 0);
+console.log(result.join("\n"));
