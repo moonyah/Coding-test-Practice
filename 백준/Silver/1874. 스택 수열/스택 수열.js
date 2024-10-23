4e65116b038026642e528b3ad85c0af2b1536a44
MunYong <51106050+moonyah@users.noreply.github.com>
2024-10-24 00:56:27 +0900

    [Silver II] Title: 스택 수열, Time: 224 ms, Memory: 31872 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -8,25 +8,27 @@ for (let i = 1; i < 1 + n; i++) {
 let tempStack = [];
 let inNum = 1;
 let answer = "";
+let index = 0;
 
 while (true) {
-  if (inNum > n && tempStack[tempStack.length - 1] !== arr[0]) {
-    console.log("NO");
-    return;
+  if (inNum > n) {
+    if (tempStack[tempStack.length - 1] !== arr[index]) {
+      console.log("NO");
+      return;
+    }
+    if (tempStack.length === 0) {
+      console.log(answer);
+      return;
+    }
   }
-  if (inNum > n && tempStack.length === 0) {
-    console.log(answer);
-    return;
-  }
-  if (tempStack[tempStack.length - 1] === arr[0]) {
+  
+  if (tempStack[tempStack.length - 1] === arr[index]) {
     tempStack.pop();
     answer += "-" + "\n";
-    arr.shift();
-    inNum--;
+    index++;
   } else {
     tempStack.push(inNum);
     answer += "+" + "\n";
+    inNum++;
   }
-
-  inNum++;
 }
