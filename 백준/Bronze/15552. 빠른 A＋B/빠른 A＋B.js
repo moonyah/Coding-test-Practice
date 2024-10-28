f96821ae07aef6c0085ff366accd6cce35aaaa5a
MunYong <51106050+moonyah@users.noreply.github.com>
2024-10-29 01:52:57 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1180 ms, Memory: 168440 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,12 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let testCase = Number(input[0]);
+let answer = "";
+
+for (let i = 1; i <= testCase; i++) {
+  let data = input[i].split(" ");
+  let a = Number(data[0]);
+  let b = Number(data[1]);
+  answer += a + b + "\n";
+}
+
+console.log(answer);
