88d865774df40feaa245333faf60a16bde153454
MunYong <51106050+moonyah@users.noreply.github.com>
2024-10-29 02:40:46 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 108 ms, Memory: 11172 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.js
@@ -0,0 +1,28 @@
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+let N = Number(input[0]);
+for (let i = 1; i < N; i++) {
+  let temp = "";
+  for (let j = 0; j < i; j++) {
+    temp += "*";
+  }
+  for (let j = 0; j < 2 * (N - i); j++) {
+    temp += " ";
+  }
+  for (let j = 0; j < i; j++) {
+    temp += "*";
+  }
+  console.log(temp);
+}
+for (let i = 0; i < N; i++) {
+  let temp = "";
+  for (let j = 0; j < N - i; j++) {
+    temp += "*";
+  }
+  for (let j = 0; j < 2 * i; j++) {
+    temp += " ";
+  }
+  for (let j = 0; j < N - i; j++) {
+    temp += "*";
+  }
+  console.log(temp);
+}
