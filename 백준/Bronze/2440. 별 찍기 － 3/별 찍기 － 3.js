551e108cdd0d984a09e4a866abca5159cf719201
MunYong <51106050+moonyah@users.noreply.github.com>
2024-10-29 01:57:29 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 104 ms, Memory: 9628 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.js
@@ -0,0 +1,9 @@
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+let N = Number(input[0]);
+for (let i = 0; i < N; i++) {
+  let temp = "";
+  for (let j = i; j < N; j++) {
+    temp += "*";
+  }
+  console.log(temp);
+}
