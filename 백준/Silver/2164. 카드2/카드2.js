b31c01cbf1ca911935c020d51ab88b3abb140f3e
MunYong <51106050+moonyah@users.noreply.github.com>
2024-10-30 02:21:36 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 32852 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.js
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -3,15 +3,12 @@ let N = Number(input[0]);
 let queue = Array(N)
   .fill(1)
   .map((a, b) => (a = b + 1));
-
 let rest = N;
 let startIdx = 0;
 while (rest > 1) {
-  // queue.shift();
   startIdx++;
   startIdx++;
   queue.push(queue[startIdx - 1]);
   rest--;
-  // console.log(queue);
 }
 console.log(queue.pop());
