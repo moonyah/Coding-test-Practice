753fb0806c6212c2458a9bdb9716b61b5f38a728
MunYong <51106050+moonyah@users.noreply.github.com>
2024-11-10 16:53:30 +0900

    [Silver I] Title: 미로 탐색, Time: 184 ms, Memory: 12264 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,48 +1,38 @@
 let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
 let [N, M] = input[0].split(" ").map(Number);
-input = input.slice(1).map((line) => line.trim().split(""));
+let maze = [];
+for (let i = 1; i < N + 1; i++) {
+  maze.push(input[i].trim().split("").map(Number));
+}
+
 let visited = Array.from({ length: N }, () => Array(M).fill(false));
+let queue = [[0, 0, 1]];
+visited[0][0] = true;
 let direction = [
   [-1, 0],
   [1, 0],
   [0, -1],
   [0, 1],
 ];
-
-function bfs() {
-  let queue = [[0, 0]];
-  visited[0][0] = true;
-  let distance = Array.from({ length: N }, () => Array(M).fill(0));
-  distance[0][0] = 1;
-
-  while (queue.length > 0) {
-    const [curX, curY] = queue.shift();
-
-    for (let [dx, dy] of direction) {
-      let newX = curX + dx;
-      let newY = curY + dy;
-
-      if (newX === N - 1 && newY === M - 1) {
-        return distance[curX][curY] + 1;
-      }
-
-      if (
-        newX >= 0 &&
-        newX < N &&
-        newY >= 0 &&
-        newY < M &&
-        !visited[newX][newY] &&
-        input[newX][newY] === "1"
-      ) {
-        visited[newX][newY] = true;
-        distance[newX][newY] = distance[curX][curY] + 1;
-        queue.push([newX, newY]);
+while (queue.length > 0) {
+  let [curY, curX, count] = queue.shift();
+  for (let [y, x] of direction) {
+    let newY = curY + y;
+    let newX = curX + x;
+    if (
+      0 <= newY &&
+      newY < N &&
+      0 <= newX &&
+      newX < M &&
+      maze[newY][newX] === 1 &&
+      visited[newY][newX] === false
+    ) {
+      visited[newY][newX] = true;
+      queue.push([newY, newX, count + 1]);
+      if (newY === N - 1 && newX === M - 1) {
+        console.log(count + 1);
+        return;
       }
     }
   }
-
-  return -1;
 }
-
-let result = bfs();
-console.log(result);
