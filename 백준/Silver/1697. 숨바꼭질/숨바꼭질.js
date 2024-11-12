f4c857f5e4844931d6f493bff4c16788fdc13793
MunYong <51106050+moonyah@users.noreply.github.com>
2024-11-12 17:57:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 256 ms, Memory: 31440 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,31 +1,28 @@
 const input = require("fs").readFileSync("dev/stdin").toString().split("\n");
-const [N, K] = input[0].split(" ").map(Number);
+let [N, K] = input[0].split(" ").map(Number);
 
-let maxLimit = Math.max(N, K) * 2 + 1;
-let visited = Array(maxLimit).fill(false);
-
-function bfs(N, K) {
-  let queue = [[N, 0]];
-  visited[N] = true;
-  let index = 0;
-
-  while (index < queue.length) {
-    let [currentPos, currentTime] = queue[index++];
-
-    if (currentPos === K) {
-      return currentTime;
-    }
-
-    let nextPositions = [currentPos - 1, currentPos + 1, currentPos * 2];
-    for (let nextPos of nextPositions) {
-      if (nextPos >= 0 && nextPos < maxLimit && !visited[nextPos]) {
-        queue.push([nextPos, currentTime + 1]);
-        visited[nextPos] = true;
+let visited = Array(100001).fill(false);
+if (N === K) {
+  console.log(0);
+  return;
+}
+let queue = [[N, 0]];
+visited[N] = true;
+let index = 0;
+while (index < queue.length) {
+  let [cur, count] = queue[index++];
+  for (let [nextCur, nextCount] of [
+    [cur * 2, count + 1],
+    [cur - 1, count + 1],
+    [cur + 1, count + 1],
+  ]) {
+    if (0 <= nextCur && nextCur < 100001 && visited[nextCur] === false) {
+      if (nextCur === K) {
+        console.log(nextCount);
+        return;
       }
+      visited[nextCur] = true;
+      queue.push([nextCur, nextCount]);
     }
   }
-  return -1;
 }
-
-let answer = bfs(N, K);
-console.log(answer);
