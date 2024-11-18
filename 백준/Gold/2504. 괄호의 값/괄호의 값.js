04db19c7b9cf6347f885d7b00a6c2a313400da6b
MunYong <51106050+moonyah@users.noreply.github.com>
2024-11-18 19:21:17 +0900

    [Gold V] Title: 괄호의 값, Time: 108 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.js
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.js
@@ -1,41 +1,39 @@
-const input = require("fs").readFileSync("dev/stdin").toString().split("\n");
+let input = require("fs").readFileSync("dev/stdin").toString().split("\n");
 let str = input[0];
+
 let stack = [];
 let answer = 0;
 let temp = 1;
-
 for (let i = 0; i < str.length; i++) {
   if (str[i] === "(") {
-    temp *= 2;
     stack.push("(");
-  } else if (str[i] === "[") {
-    temp *= 3;
-    stack.push("[");
+    temp *= 2;
   } else if (str[i] === ")") {
+    if (str[i - 1] === "(") {
+      answer += temp;
+    }
     if (stack.length === 0 || stack[stack.length - 1] !== "(") {
       answer = 0;
       break;
     }
-    if (str[i - 1] === "(") {
+    stack.pop();
+    temp /= 2;
+  } else if (str[i] === "[") {
+    stack.push("[");
+    temp *= 3;
+  } else if (str[i] === "]") {
+    if (str[i - 1] === "[") {
       answer += temp;
     }
-    temp /= 2;
-    stack.pop();
-  } else if (str[i] === "]") {
     if (stack.length === 0 || stack[stack.length - 1] !== "[") {
       answer = 0;
       break;
     }
-    if (str[i - 1] === "[") {
-      answer += temp;
-    }
-    temp /= 3;
     stack.pop();
+    temp /= 3;
   }
 }
-
 if (stack.length !== 0) {
-  console.log(0);
-} else {
-  console.log(answer);
+  answer = 0;
 }
+console.log(answer);
