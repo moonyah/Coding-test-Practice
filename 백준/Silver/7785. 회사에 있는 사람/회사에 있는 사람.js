4e415b1d03aaed2d5047beeecc195166c0eb57ee
MunYong <51106050+moonyah@users.noreply.github.com>
2025-01-10 14:48:58 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 3760 ms, Memory: 58720 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.js
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.js
@@ -1,6 +1,5 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 let n = Number(input[0]);
-// console.log(n);
 let peopleList = {};
 for (let i = 1; i < n + 1; i++) {
   let [name, workState] = input[i].trim().split(" ");
@@ -11,17 +10,12 @@ for (let i = 1; i < n + 1; i++) {
     peopleList[name] = 0;
   }
 }
-// console.log(peopleList);
-// { Baha: 0, Askar: 1, Artem: 1 }
-
 let enterPeople = [];
-// for in 문 사용
 for (let key in peopleList) {
   if (peopleList[key]) {
     enterPeople.push(key);
   }
 }
-// console.log(enterPeople);
 enterPeople.sort().reverse();
 for (let i of enterPeople) {
   console.log(i);
