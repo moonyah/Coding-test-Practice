906a3ab636893432ff26247d0560bb9a78cad9a8
MunYong <51106050+moonyah@users.noreply.github.com>
2025-03-02 01:35:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 10164 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -1,43 +1,32 @@
 let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 let N = Number(input[0]);
-// console.log(N);
 // 이중 배열에 넣기
 let arr = [];
 for (let i = 1; i < N + 1; i++) {
   arr.push([...input[i].trim()]);
 }
-// console.log(arr);
-
-let visited = Array.from({ length: N }, () => Array(N).fill(false));
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if (arr[i][j] == 0) {
-      visited[i][j] = true;
-    }
-  }
-}
-// console.log(visited);
 
 // 배열 순회
 let answer = 0;
 let areas = [];
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < N; j++) {
-    if (!visited[i][j]) {
+    if (arr[i][j] == "1") {
       let area = bfs(i, j);
       areas.push(area);
       answer++;
     }
   }
 }
+
 // output
 console.log(answer);
 areas.sort((a, b) => a - b);
-areas.map((a) => console.log(a));
+console.log(areas.join("\n"));
 
 function bfs(i, j) {
   let queue = [[i, j]]; // y, x
-  visited[i][j] = true;
+  arr[i][j] = "0";
   let direction = [
     [-1, 0],
     [1, 0],
@@ -52,8 +41,8 @@ function bfs(i, j) {
       let ny = cy + y;
       let nx = cx + x;
       if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
-      if (!visited[ny][nx]) {
-        visited[ny][nx] = true;
+      if (arr[ny][nx] == 1) {
+        arr[ny][nx] = "0";
         queue.push([ny, nx]);
         area++;
       }
